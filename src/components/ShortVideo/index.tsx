b97e021e9c5422c0be4bf77966c9d88e41e4e46f
ouliana <ouliana.kotik@gmail.com>
2023-10-12 15:55:40 +0300

    fixed mixed content

--- src/components/ShortVideo/index.tsx
+++ src/components/ShortVideo/index.tsx
@@ -1,7 +1,7 @@
 import Container from "./StyledContainer";
 
 const ShortVideo = ({videoId}: {videoId:string}) => {
-  const src = `http://www.youtube.com/embed/${videoId}`;
+  const src = `https://www.youtube.com/embed/${videoId}`;
   return <Container>
     <iframe className="w-full h-full" allowFullScreen={true} src={src}></iframe>
   </Container>
