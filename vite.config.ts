de265a8999b3b6a33db44dcd80594379cc2052c5
ouliana <ouliana.kotik@gmail.com>
2023-11-01 22:35:43 +0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/api': 'https://serge-govorun.onrender.com' } },
+  server: {
+    proxy: { '/api': `https://serge-govorun.onrender.com:${process.env.PORT}` },
+  },
 });
