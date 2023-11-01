e24539f358e5865d316ffeb5396691c4b8011816
ouliana <ouliana.kotik@gmail.com>
2023-11-02 00:21:16 +0300

    helmet

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,24 @@ import { Outlet, RouterProvider } from 'react-router-dom';
 import { router } from './App';
 import './i18n/config';
 import './index.css';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <Outlet />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+      <Helmet>
+        <meta
+          httpEquiv='Content-Security-Policy'
+          content={`
+                      default-src 'self';
+                      script-src 'self';
+                      img-src 'self' https://img.youtube.com;
+                      media-src https://www.youtube.com;
+                `}
+        ></meta>
+      </Helmet>
+      <Outlet />
+    </HelmetProvider>
   </React.StrictMode>
 );
