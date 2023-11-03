a09750f1bca26ffdd09982e26fd809f4440e09c8
ouliana <ouliana.kotik@gmail.com>
2023-11-03 11:36:29 +0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-let proxy;
-switch (process.env.TARGET) {
-  case 'dev':
-    proxy = { '/api': `http://localhost:3001` };
-    break;
-  case 'my':
-    proxy = { '/api': `http://oulianakotik.com` };
-    break;
-  case 'render':
-    proxy = {
-      '/api': `https://serge-govorun.onrender.com:${process.env.PORT}`,
-    };
-    break;
-  default:
-    proxy = { '/api': `http://localhost:3001` };
-    break;
-}
+// let proxy;
+// switch (process.env.TARGET) {
+//   case 'dev':
+//     proxy = { '/api': `http://localhost:3001` };
+//     break;
+//   case 'my':
+//     proxy = { '/api': `http://oulianakotik.com` };
+//     break;
+//   case 'render':
+//     proxy = {
+//       '/api': `https://serge-govorun.onrender.com:${process.env.PORT}`,
+//     };
+//     break;
+//   default:
+//     proxy = { '/api': `http://localhost:3001` };
+//     break;
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy,
+    proxy: { '/api': `http://oulianakotik.com` },
   },
 });
