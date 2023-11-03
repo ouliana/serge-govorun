aa3287f5c87baf5773527b4590fa3d58fd681f8c
ouliana <ouliana.kotik@gmail.com>
2023-11-03 20:53:19 +0300

    test commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    // proxy: { '/api': `http://localhost:3001` },
     proxy: { '/api': `http://oulianakotik.com` },
   },
 });
