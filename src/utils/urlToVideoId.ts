38bdd195575f25bc9611f852f608c49310e8cde3
ouliana <ouliana.kotik@gmail.com>
2023-11-04 16:48:07 +0300

    regexp

--- src/utils/urlToVideoId.ts
+++ src/utils/urlToVideoId.ts
@@ -1,5 +1,6 @@
 export const urlToVideoId = (url: string) => {
-  const regEx = /(?<=v=|v\/|vi=|vi\/|youtu.be\/|shorts\/)[a-zA-Z0-9_-]{11}/i;
+  // const regEx = /(?<=v=|v\/|vi=|vi\/|youtu.be\/|shorts\/)[a-zA-Z0-9_-]{11}/i;
+  const regEx = /(v=|v\/|vi=|vi\/|youtu.be\/|shorts\/)[a-zA-Z0-9_-]{11}/i;
   const part = url.match(regEx);
   console.log('url: ', url);
   console.log(part);
