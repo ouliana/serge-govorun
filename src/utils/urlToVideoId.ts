edabd4eb457a69d7679046e852e8455c4f492441
ouliana <ouliana.kotik@gmail.com>
2023-11-04 16:42:08 +0300

    console.log

--- src/utils/urlToVideoId.ts
+++ src/utils/urlToVideoId.ts
@@ -1,7 +1,8 @@
 export const urlToVideoId = (url: string) => {
-  const part = url.match(
-    /(?<=v=|v\/|vi=|vi\/|youtu.be\/|shorts\/)[a-zA-Z0-9_-]{11}/i
-  );
+  const regEx = /(?<=v=|v\/|vi=|vi\/|youtu.be\/|shorts\/)[a-zA-Z0-9_-]{11}/i;
+  const part = url.match(regEx);
+  console.log('url: ', url);
+  console.log(part);
   if (part?.length) {
     return part[0];
   }
