2a0a56836e9db98a598345c82329c21e2480885c
ouliana <ouliana.kotik@gmail.com>
2023-11-06 00:16:25 +0300

    removed onEnd hamdler

--- src/server/index.ts
+++ src/server/index.ts
@@ -25,7 +25,7 @@ app.use(express.static(process.cwd() + '/dist'));
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 if (process.env.SSL === 'no_ssl') {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
